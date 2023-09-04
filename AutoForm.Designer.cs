771e5d478507156803bb18c3868ded17070918c0
Eagle9461 <Eagle9461@outlook.com>
2023-09-04 12:02:24 -0300

    no message

---
+++ AutoForm.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace SupportInterview
+{
+    partial class AutoForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtResult = new TextBox();
+            txtPrompt = new TextBox();
+            splitContainer = new SplitContainer();
+            ((System.ComponentModel.ISupportInitialize)splitContainer).BeginInit();
+            splitContainer.Panel1.SuspendLayout();
+            splitContainer.Panel2.SuspendLayout();
+            splitContainer.SuspendLayout();
+            SuspendLayout();
+            // 
+            // txtResult
+            // 
+            txtResult.BackColor = Color.Black;
+            txtResult.Dock = DockStyle.Fill;
+            txtResult.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            txtResult.ForeColor = Color.White;
+            txtResult.Location = new Point(0, 0);
+            txtResult.Multiline = true;
+            txtResult.Name = "txtResult";
+            txtResult.Size = new Size(742, 266);
+            txtResult.TabIndex = 0;
+            txtResult.KeyUp += AutoForm_KeyUp;
+            // 
+            // txtPrompt
+            // 
+            txtPrompt.BackColor = Color.Black;
+            txtPrompt.Dock = DockStyle.Fill;
+            txtPrompt.ForeColor = Color.White;
+            txtPrompt.Location = new Point(0, 0);
+            txtPrompt.Multiline = true;
+            txtPrompt.Name = "txtPrompt";
+            txtPrompt.Size = new Size(742, 137);
+            txtPrompt.TabIndex = 2;
+            txtPrompt.KeyUp += AutoForm_KeyUp;
+            // 
+            // splitContainer
+            // 
+            splitContainer.Dock = DockStyle.Fill;
+            splitContainer.Location = new Point(0, 0);
+            splitContainer.Name = "splitContainer";
+            splitContainer.Orientation = Orientation.Horizontal;
+            // 
+            // splitContainer.Panel1
+            // 
+            splitContainer.Panel1.Controls.Add(txtPrompt);
+            splitContainer.Panel1.RightToLeft = RightToLeft.No;
+            // 
+            // splitContainer.Panel2
+            // 
+            splitContainer.Panel2.Controls.Add(txtResult);
+            splitContainer.Panel2.RightToLeft = RightToLeft.No;
+            splitContainer.RightToLeft = RightToLeft.No;
+            splitContainer.Size = new Size(742, 407);
+            splitContainer.SplitterDistance = 137;
+            splitContainer.TabIndex = 3;
+            splitContainer.SplitterMoved += splitContainer1_SplitterMoved;
+            // 
+            // AutoForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(742, 407);
+            Controls.Add(splitContainer);
+            KeyPreview = true;
+            Name = "AutoForm";
+            Text = "AutoForm";
+            FormClosed += AutoForm_FormClosed;
+            KeyPress += AutoForm_KeyPress;
+            KeyUp += AutoForm_KeyUp;
+            splitContainer.Panel1.ResumeLayout(false);
+            splitContainer.Panel1.PerformLayout();
+            splitContainer.Panel2.ResumeLayout(false);
+            splitContainer.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)splitContainer).EndInit();
+            splitContainer.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private TextBox txtResult;
+        private TextBox txtPrompt;
+        private SplitContainer splitContainer;
+    }
+}
---
+++ AutoForm.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SupportInterview
+{
+    enum Status
+    {
+        NOACTION,
+        SENDING,
+        RECEVING,
+        RECEIVED
+    }
+    public partial class AutoForm : Form
+    {
+        Status cur_status = Status.NOACTION;
+        string prompt = string.Empty;
+        string lastprompt = string.Empty;
+        ChatGPTClient chatGPTClient;
+        Thread aiBot;
+        bool isRunning = true;
+        public AutoForm()
+        {
+            InitializeComponent();
+            // Replace with your ChatGPT API key
+            string apiKey = "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";
+            // Create a ChatGPTClient instance with the API key
+            chatGPTClient = new ChatGPTClient(apiKey);
+
+            ThreadStart aiBotStart = new ThreadStart(run);
+
+            aiBot = new Thread(aiBotStart);
+            aiBot.Start();
+        }
+        public void run()
+        {
+
+            // Display a welcome message
+            PrintMessage("Welcome to the SupportInterviewBot!");
+
+            // Enter a loop to take user input and display chatbot responses
+            while (isRunning)
+            {
+                // Prompt the user for input
+                //PrintMessage("You: ");
+                prompt = txtPrompt.Text;
+                //cur_status = Status.SENDING;
+                Thread.Sleep(1000);
+
+                if (cur_status != Status.SENDING || prompt == string.Empty || prompt == lastprompt)
+                {
+                    continue;
+                }
+
+                lastprompt = prompt;
+                string initialPrompt = "Find questions from following text and answer.\n";
+                prompt = initialPrompt + prompt;
+
+                // Send the user's input to the ChatGPT API and receive a response
+                string response = chatGPTClient.SendMessage(prompt);
+                cur_status = Status.RECEIVED;
+                //txtPrompt.Text = "";
+                // Display the chatbot's response
+                PrintMessage(response);
+            }
+        }
+        public void PrintMessage(string message)
+        {
+            txtResult.Text = message;
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            Geneate();
+        }
+
+        private void splitContainer1_SplitterMoved(object sender, SplitterEventArgs e)
+        {
+            txtPrompt.Width = splitContainer.Panel1.Width;
+            txtPrompt.Height = splitContainer.Panel1.Height;
+            txtResult.Width = splitContainer.Panel2.Width;
+            txtResult.Height = splitContainer.Panel2.Height;
+        }
+
+        private void AutoForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            isRunning = false;
+            //aiBot.Suspend();
+        }
+
+        private void AutoForm_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode.ToString() == "ControlKey")
+                Geneate();
+        }
+        private void Geneate()
+        {
+            string initialPrompt = "Find questions from following text and answer.\n";
+            prompt = initialPrompt + txtPrompt.Text;
+            cur_status = Status.SENDING;
+        }
+        private void AutoForm_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+    }
+}
